5fdd70a9f2b5c4a6fc02ecbeb6afb913ef9f3306
Anil <kanilreddy867@gmail.com>
2023-11-08 00:05:14 +0530

    working on the update to favourites section.

--- public/js/index.js
+++ public/js/index.js
@@ -19,3 +19,74 @@ form.addEventListener("submit", (e) => {
       console.error("Error:", error);
     });
 });
+
+let table = document.querySelector("#table");
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("http://localhost:8000/info/all", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((re) => {
+      const data = re.data;
+      console.log(data);
+      data.forEach((e) => {
+        const tr = document.createElement("tr");
+        const td1 = document.createElement("td");
+        td1.textContent = e.domain;
+        const td2 = document.createElement("td");
+        td2.textContent = e.wordCount;
+        const td3 = document.createElement("td");
+        td3.textContent = e.favorite;
+        const td4 = document.createElement("td");
+        td4.textContent = arrayToTdString(e.webLinks, 4);
+        const td5 = document.createElement("td");
+        td5.textContent = arrayToTdString(e.mediaLinks, 4);
+        const td6 = document.createElement("td");
+        // Create the "Remove" button
+        const removeButton = document.createElement("button");
+        removeButton.textContent = "Remove";
+        removeButton.addEventListener("click", () => {
+          // Handle the removal action here
+          // You can remove the corresponding row or perform other actions
+        });
+
+        // Create the "Add to Fav" button
+        const addToFavButton = document.createElement("button");
+        addToFavButton.textContent = "Add to Fav";
+        addToFavButton.addEventListener("click", () => {
+          const newFavoriteStatus = !e.favorite;
+          updateFavoriteStatus(e.id, newFavoriteStatus);
+      });
+        td6.append(removeButton,addToFavButton)
+
+        tr.append(td1, td2, td3, td4, td5, td6);
+        table.append(tr);
+      });
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+});
+
+// function to convert items in an array to string.
+function arrayToTdString(arr, limit) {
+  return arr.slice(0, limit).join(", ");
+}
+// Create a function to handle updating the "favorite" status
+function updateFavoriteStatus(id, isFavorite) {
+  fetch("http://localhost:8000/info/update", {
+      method: "PATCH",
+      headers: {
+          "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id, favorite: isFavorite }),
+  })
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => {
+      console.error("Error updating favorite status:", error);
+  });
+}
--- routes/domainRouter.js
+++ routes/domainRouter.js
@@ -54,4 +54,42 @@ router.post("/scrape", async (req, res) => {
   }
 });
 
+router.get("/all", async (req, res) => {
+  try {
+    const allDomains = await domainModel.find();
+
+    res.status(200).json({ message: "Retrived data", data: allDomains });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: err.message });
+  }
+});
+
+router.patch("/update", async (req, res) => {
+  console.log('recieved');
+  try {
+    const { id, favorite } = req.body;
+    const updatedData = await domainModel.findByIdAndUpdate(
+      id,
+      { favorite: favorite },
+      { new: true }
+    );
+
+    if (!updatedData) {
+      return res.status(404).json({ message: "Data not found" });
+    }
+
+    if (updatedData.favorite) {
+      res.json({ message: "Already in favorites" });
+    } else {
+      res.json({ favorite: updatedData.favorite });
+    }
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: err.message });
+  }
+});
+
 module.exports = { router };
